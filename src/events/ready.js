2a43d4086a7158e803f7f9b2e9e807ec1209cc75
wicsse <rihambelenus09@gmail.com>
2026-05-26 19:28:52 +0000

    fix: ready -> clientReady

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,7 @@
 const { initInviteCache } = require("../handlers/inviteLogger");
 
 module.exports = {
-  name: "ready",
+  name: "clientReady",
   once: true,
   async execute(client) {
     console.log(`✅ Connecté en tant que ${client.user.tag}`);
