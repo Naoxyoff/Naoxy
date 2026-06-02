f2ad37e2df88554a0e9610e6e8f59545713b5343
wicsse <rihambelenus09@gmail.com>
2026-06-02 00:25:52 +0000

    fix: add welcome and autorole to guildMemberAdd

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,8 +1,12 @@
 const { logInvite } = require("../handlers/inviteLogger");
+const { sendWelcome } = require("../handlers/welcome");
+const autoRoleHandler = require("../handlers/autoRoleHandler");
 
 module.exports = {
   name: "guildMemberAdd",
   async execute(member) {
     await logInvite(member);
+    await sendWelcome(member);
+    await autoRoleHandler.execute(member);
   },
 };
