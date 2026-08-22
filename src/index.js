2e0cffbbaa7aec8e36b1e667f0aab3d4dcebe4d5
wicsse <rihambelenus09@gmail.com>
2026-08-22 18:55:36 +0000

    Chemin des vues corrigé !

--- src/index.js
+++ src/index.js
@@ -179,4 +179,4 @@ startBot();
 
 // Configuration du moteur de vues EJS pour le web
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'web/views'));
+app.set('views', path.join(__dirname, 'src/web/views'));
