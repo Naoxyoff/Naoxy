663756008d52606cc50957e79b410a2b0c884d92
wicsse <rihambelenus09@gmail.com>
2026-08-22 20:41:12 +0000

    Clean up index.js and add root render route properly

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.render("index");
-});
 
 
 (async () => {
@@ -237,7 +234,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
     console.log("🌐 Serveur Express en écoute sur le port " + PORT);
 });
-
 
 app.get('/', (req, res) => {
     res.render('index');
